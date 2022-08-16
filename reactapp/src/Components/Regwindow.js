2880b821b0c62044b96326488706c2c709165012
gferrari18 <gferrari18@my.whitworth.edu>
2022-08-15 23:43:57 -0700

    finished

--- reactapp/src/Components/Regwindow.js
+++ reactapp/src/Components/Regwindow.js
@@ -2,13 +2,31 @@ import React from "react";
 import "../App.css";
 import "./Regwindow.css";
 import { Button } from "./Button";
+import axios from "axios";
+import { useRef } from "react";
 
 function Regwindow() {
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
+
+  function Login() {
+    var url =
+      "http://127.0.0.1:5000/login/" +
+      inputRef1.current.value +
+      "/" +
+      inputRef2.current.value +
+      "/" +
+      inputRef3.current.value;
+    axios.get(url);
+  }
+
   return (
     <div className="Regwindow-container">
       <form>
         <div>
           <input
+            ref={inputRef1}
             className="Regwindow-input"
             name="email"
             type="email"
@@ -17,6 +35,7 @@ function Regwindow() {
         </div>
         <div>
           <input
+            ref={inputRef2}
             className="Regwindow-input"
             name="Username"
             type="Username"
@@ -25,6 +44,7 @@ function Regwindow() {
         </div>
         <div>
           <input
+            ref={inputRef3}
             className="Regwindow-input"
             name="Password"
             type="Password"
@@ -33,6 +53,7 @@ function Regwindow() {
         </div>
         <div>
           <Button
+            onClick={Login}
             className="btns"
             buttonStyle="btn--outline"
             buttonSize="btn--large"
